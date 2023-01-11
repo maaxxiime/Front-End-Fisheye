a8a6605b406b35bb8452a18608444b16243053ee
Maxime <maximeringeval@orange.fr>
2023-01-11 10:10:22 +0100

    Dom element page accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,35 @@
+// Balises sémentiques Header, Nav, Main, Section, Article, Aside, Footer
 function photographerFactory(data) {
     const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/Sample/photographersIdPhotos/${portrait}`;
+    console.log(data)
 
     function getUserCardDOM() {
+
+        // DOM element
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const localisation = document.createElement('p');
+        const tagLine = document.createElement('p');
+        const prices = document.createElement('p');
+
+
+        img.setAttribute("src", picture)
+        img.setAttribute('alt', `lien vers la page du photographe ${name}`)
         h2.textContent = name;
+        localisation.textContent = `${city}, ${country}`;
+        tagLine.textContent = tagline;
+        prices.textContent = `${price}€/jour`
+
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(localisation)
+        article.appendChild(tagLine)
+        article.appendChild(prices)
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, id, city, country, tagline, price, getUserCardDOM }
 }
