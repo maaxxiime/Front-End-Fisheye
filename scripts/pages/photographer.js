b5667355673d8fdead8531147c68625921a12008
Maxime <maximeringeval@orange.fr>
2023-01-11 14:04:07 +0100

    section photographer / css

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,8 +1,7 @@
-async function getId() {
-
+function getId() {
   //récupérer le params passé en url
   const params = window.location.href.split("=")[1];
-  const ID = parseInt(params)
+  const ID = parseInt(params);
 
   //créer un tableau de photgraphe
   let photographers = [];
@@ -16,7 +15,32 @@ async function getId() {
       //parcours le tableau photographers
       photographers.forEach((photographer) => {
         if (photographer.id === ID) {
-          console.log(photographer);
+
+
+          const picture = `assets/Sample/photographersIdPhotos/${photographer.portrait}`;
+          const section = document.querySelector(".photograph-header");
+          const divFlex = document.createElement("div");
+          const title = document.createElement("h1");
+          const localisation = document.createElement("p");
+          const tagLine = document.createElement("p");
+          const img = document.createElement("img");
+
+
+          divFlex.setAttribute('class', "divCollumn")
+
+
+          title.textContent = photographer.name;
+          localisation.textContent = `${photographer.city}, ${photographer.country}`;
+          tagLine.textContent = photographer.tagline;
+          img.setAttribute("src", picture);
+
+
+          section.appendChild(divFlex);
+          divFlex.appendChild(title);
+          divFlex.appendChild(localisation);
+          divFlex.appendChild(tagLine);
+          section.appendChild(img);
+
           return photographer;
         }
       });
