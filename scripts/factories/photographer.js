3b0c2e8a3b2f1694f0a551b78bbbeb8f4496d3c3
Maxime <maximeringeval@orange.fr>
2023-01-11 12:11:49 +0100

    css / get data page photographe

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,35 +1,44 @@
 // Balises sémentiques Header, Nav, Main, Section, Article, Aside, Footer
 function photographerFactory(data) {
-    const { name, id, city, country, tagline, price, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/Sample/photographersIdPhotos/${portrait}`;
-    console.log(data)
+  const picture = `assets/Sample/photographersIdPhotos/${portrait}`;
 
-    function getUserCardDOM() {
+  function getUserCardDOM() {
 
-        // DOM element
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        const h2 = document.createElement( 'h2' );
-        const localisation = document.createElement('p');
-        const tagLine = document.createElement('p');
-        const prices = document.createElement('p');
-
-
-        img.setAttribute("src", picture)
-        img.setAttribute('alt', `lien vers la page du photographe ${name}`)
-        h2.textContent = name;
-        localisation.textContent = `${city}, ${country}`;
-        tagLine.textContent = tagline;
-        prices.textContent = `${price}€/jour`
+    // DOM element
+    const article = document.createElement("article");
+    const link = document.createElement("a");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const localisation = document.createElement("p");
+    const tagLine = document.createElement("p");
+    const prices = document.createElement("p");
 
 
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(localisation)
-        article.appendChild(tagLine)
-        article.appendChild(prices)
-        return (article);
-    }
-    return { name, picture, id, city, country, tagline, price, getUserCardDOM }
+    // Set attribute
+    article.setAttribute('aria-label', `Photographe ${name}`)
+    link.setAttribute("alt", name);
+    link.setAttribute("href", `./photographer.html?id=${id}`)
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", name);
+
+
+    // Text content
+    h2.textContent = name;
+    localisation.textContent = `${city}, ${country}`;
+    tagLine.textContent = tagline;
+    prices.textContent = `${price}€/jour`;
+
+    // Appen child
+    article.appendChild(link);
+    link.appendChild(img);
+    link.appendChild(h2);
+    article.appendChild(localisation);
+    article.appendChild(tagLine);
+    article.appendChild(prices);
+
+    return article;
+  }
+  return { name, picture, id, city, country, tagline, price, getUserCardDOM };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,23 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getId() {
+  //récupérer le params passé en url
+  const ID = window.location.href.split("=")[1];
+
+  //créer un tableau de photgraphe
+  let photographers = [];
+
+  // fetch la data
+  return fetch("../data/photographers.json")
+    .then((res) => res.json())
+    .then((data) => {
+      photographers = data.photographers;
+
+      //parcours le tableau photographers
+      photographers.forEach((photographer) => {
+        if (photographer.id == ID) {
+          console.log(photographer);
+          return photographer;
+        }
+      });
+    });
+}
+getId();
