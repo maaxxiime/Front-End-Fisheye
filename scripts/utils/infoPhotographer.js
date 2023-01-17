f134b6151a38b06f6275593c1a690d0fa545926e
Maxime <maximeringeval@orange.fr>
2023-01-17 11:42:41 +0100

    aside photograph-infos

---
+++ scripts/utils/infoPhotographer.js
@@ -0,0 +1,42 @@
+async function displayInfos() {
+
+    const param = window.location.href.split("=")[1].split("?")[0];
+    const ID = parseInt(param);
+    
+    
+    const data = await fetch("../data/photographers.json");
+    const resultat = await data.json();
+    const photographers = resultat.photographers;
+    const mediasData = resultat.media;
+    const user = photographers.filter((photographer) => photographer.id === ID)[0];
+    const mediasUser = mediasData.filter((media) => media.photographerId === ID);
+
+    for(let i = 0 ; i < mediasUser.length ; i++) {
+    }
+    mediasUser.forEach((media) => {
+    })
+
+    const body = document.getElementById('body')
+    const aside = document.createElement("aside");
+    const divLike = document.createElement("div");
+    const divPrice = document.createElement("div");
+    const likesCount = document.createElement("p");
+    const heart = document.createElement("img");
+    const price = document.createElement("p");
+
+    aside.setAttribute("class", "photograph-infos");
+    likesCount.textContent = "106564";
+    likesCount.setAttribute("class", "like-count");
+    heart.setAttribute("src", "./assets/icons/heart-solid.svg")
+    heart.setAttribute("class", "heart-count");
+    price.textContent = `${user.price}€ / jour`
+
+    body.appendChild(aside)
+    aside.appendChild(divLike)
+    aside.appendChild(divPrice)
+    divLike.appendChild(likesCount)
+    divLike.appendChild(heart)
+    divPrice.appendChild(price)
+
+}
+displayInfos()
