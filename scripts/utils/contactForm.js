4821065cb9b835f743b581afe609b0a6911ce9f8
Maxime <maximeringeval@orange.fr>
2023-01-27 09:18:18 +0100

    accessibilité

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -3,27 +3,47 @@ const modal = document.getElementById("contact_modal");
 const params = window.location.href.split("=")[2].split("?")[0].replace("%20", " ").replace("%20", " ");
 modal.setAttribute('aria-label', `contact me ${params}`);
 
+const header = document.getElementById('header');
+const main = document.getElementById('main');
+const realisation = document.getElementById('realisation');
+const filter = document.getElementById('filter');
+const gallery = document.getElementById('gallery');
+
+
 
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    header.setAttribute("aria-hidden", "true");
+    main.setAttribute("aria-hidden", "true");
+    realisation.setAttribute("aria-hidden", "true");
+    filter.setAttribute("aria-hidden", "true");
+    gallery.setAttribute("aria-hidden", "true");
 }
 
 const closeBtn = document.getElementById('close-btn');
-
 closeBtn.addEventListener("click", function closeModal(){
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
-
+    header.setAttribute("aria-hidden", "false");
+    main.setAttribute("aria-hidden", "false");
+    realisation.setAttribute("aria-hidden", "false");
+    filter.setAttribute("aria-hidden", "false");
+    gallery.setAttribute("aria-hidden", "false");
 })
 
 closeBtn.addEventListener("keypress", (e) => {
 
     const keyName = e.key;
-    console.log(keyName)
+
     if (keyName === 'Enter') {
         const modal = document.getElementById("contact_modal");
         modal.style.display = "none";
+        header.setAttribute("aria-hidden", "false");
+        main.setAttribute("aria-hidden", "false");
+        realisation.setAttribute("aria-hidden", "false");
+        filter.setAttribute("aria-hidden", "false");
+        gallery.setAttribute("aria-hidden", "false");
     }
 
 })
