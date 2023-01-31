09aff2c2f6ac1005305e539a2861896fdb455b29
Maxime <maximeringeval@orange.fr>
2023-01-31 19:46:04 +0100

    Contact form error & css

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,57 +1,100 @@
 // Accessibilité
 const modal = document.getElementById("contact_modal");
-const params = window.location.href.split("=")[2].split("?")[0].replace("%20", " ").replace("%20", " ");
-modal.setAttribute('aria-label', `contact me ${params}`);
+const formData = document.querySelectorAll(".formData");
+const params = window.location.href
+  .split("=")[2]
+  .split("?")[0]
+  .replace("%20", " ")
+  .replace("%20", " ");
+modal.setAttribute("aria-label", `contact me ${params}`);
 
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
 }
 
-const closeBtn = document.getElementById('close-btn');
-closeBtn.addEventListener("click", function closeModal(){
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
-})
+const closeBtn = document.getElementById("close-btn");
+closeBtn.addEventListener("click", function closeModal() {
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
+});
 
 closeBtn.addEventListener("keypress", (e) => {
-    const keyName = e.key;
-    if (keyName === 'Enter') {
-        const modal = document.getElementById("contact_modal");
-        modal.style.display = "none";
-    }
+  const keyName = e.key;
+  if (keyName === "Enter") {
+    const modal = document.getElementById("contact_modal");
+    modal.style.display = "none";
+  }
+});
 
-})
+const form = document.getElementById("form");
+form.addEventListener("submit", function sendForm(e) {
+  e.preventDefault();
+
+  const prenom = document.getElementById("prenom").value;
+  const nom = document.getElementById("nom").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  let prenomIsValide = false;
+  let nomIsValide = false;
+  let emailIsValide = false;
+  let messageIsValide = false;
+
+  let res = null;
+
+  if (prenom.length < 2 || prenom == "") {
+    formData[0].dataset.errorVisible = true;
+    prenomIsValide = false;
+  } else {
+    console.log("Prénom à deux");
+    formData[0].dataset.errorVisible = false;
+    prenomIsValide = true;
+  }
+
+  if (nom.length < 2 || nom == "") {
+    formData[1].dataset.errorVisible = true;
+    nomIsValide = false;
+  } else {
+    console.log("Nom à deux");
+    formData[1].dataset.errorVisible = false;
+    nomIsValide = true;
+  }
+
+  if (email.includes("@") && email.includes(".") && email.length >= 6) {
+    console.log("Email valide");
+    formData[2].dataset.errorVisible = false;
+    emailIsValide = true;
+  } else {
+    formData[2].dataset.errorVisible = true;
+    emailIsValide = false;
+  }
+  if (message == "") {
+    formData[3].dataset.errorVisible = true;
+    messageIsValide = false;
+  } else {
+    console.log("message valide");
+    formData[3].dataset.errorVisible = false;
+    messageIsValide = true;
+  }
 
 
-const form = document.getElementById('form')
-form.addEventListener("submit", function sendForm(e){
-    e.preventDefault();
-    
-    const prenom = document.getElementById('prenom').value;
-    const nom = document.getElementById('nom').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+  if (
+    prenomIsValide === true &&
+    nomIsValide === true &&
+    emailIsValide === true &&
+    messageIsValide === true
+  ) {
+    res = {
+      prenom: prenom,
+      nom: nom,
+      email: email,
+      message: message,
+    };
 
-    let res = null
-
-    if(prenom != "" && nom!= "" && email != "" && message != "" && email.includes('@') && email.includes('.')) {
-        res = {
-            prenom: prenom,
-            nom: nom,
-            email: email,
-            message: message
-        }
-
-        const modal = document.getElementById("contact_modal");
-        modal.style.display = "none";
-
-        form.reset()
-
-    } else {
-        res = "erreur"
-    }
-
-
-    console.log(res)
-})
+    form.reset();
+  } else {
+    res = "erreur";
+  }
+  console.log(res);
+});
